98ff7a5c89d73d65431acc53a53fc0076b4ddc35
Kat Iliad <121892621+katiliad@users.noreply.github.com>
2023-02-09 18:52:07 +0200

    Initial repo commit

---
+++ vaccineyay-backend/src/main/java/com/health/vaccineyaybackend/VaccineyayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.health.vaccineyaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineyayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineyayBackendApplication.class, args);
+	}
+
+}
---
+++ vaccineyay-backend/src/test/java/com/health/vaccineyaybackend/VaccineyayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.vaccineyaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineyayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
